a84b6a27599d24ecbc0c28a35f16af2db42d9d13
KirPoz <84350768+KirPoz@users.noreply.github.com>
2021-05-22 22:41:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    date = new Date();
+
+    amount = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    sum = (amount * (1+rate/100)**years) - amount;
+
+    currentYear = date.getFullYear();
+    year = Number(currentYear) + Number(years);
+
+    jsVar = 
+        '<p> If you deposit: ' + amount + '</p>' +
+        '<p> at an interest rate of: ' + rate + '% </p>' +
+        '<p> You will receive an amount of: ' + sum.toFixed(2) + '</p>' +
+        '<p> in the year: ' + year + '</p>';
+
+    //alert(typeof currentYear +';'+ typeof years);
+
+    document.getElementById('output').innerHTML = jsVar;
+       
 }
-        
+
+function range() {
+	var rng=document.getElementById('rate'); //rng - это Input
+	var p=document.getElementById('rate_now'); // p - абзац
+	p.innerHTML=rng.value + '%';
+}
